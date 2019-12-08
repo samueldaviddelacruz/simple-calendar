51bb65d5029282d642752c7f4414b5a406932151
Samuel <alphaelena@gmail.com>
2019-12-08 14:51:42 -0400

    Added move-between-months functionality

--- src/Containers/Calendar/Calendar.js
+++ src/Containers/Calendar/Calendar.js
@@ -5,26 +5,27 @@ import {
   getDay,
   addDays,
   getDaysInMonth,
+  subMonths,
+  subDays
 } from "date-fns";
 import "./Calendar.css";
 import CalendarDay from "../../Components/CalendarDay/CalendarDay";
 
 const TOTAL_DAYS_IN_CALENDAR = 35;
 const getCalendarDaysInterval = startMonthDate => {
-  const endMonthDate = endOfMonth(Date.now());
+  const endMonthDate = endOfMonth(startMonthDate);
+  //const previousMonth = endOfMonth(subMonths(startMonthDate, 1));
+  //console.log(TOTAL_DAYS_IN_CALENDAR - getDaysInMonth(startMonthDate))
+ 
   const daysInInterval = eachDayOfInterval({
-    start: addDays(startMonthDate, getDay(startMonthDate)),
-    end: addDays(
-      endMonthDate,
-      TOTAL_DAYS_IN_CALENDAR - getDaysInMonth(startMonthDate)
-    )
+    start: subDays(startMonthDate, getDay(startMonthDate)),
+    end: addDays(endMonthDate, (TOTAL_DAYS_IN_CALENDAR - getDaysInMonth(startMonthDate)) - getDay(startMonthDate) )
   });
-
+  
   return daysInInterval;
 };
 
 const Calendar = ({ startMonthDate, reminders, onReminderSelected }) => {
-  
   const daysInInterval = getCalendarDaysInterval(startMonthDate);
   return daysInInterval.map((dayDate, indx) => {
     return (
